917f090d159b2a78a07d690dde99bee0705f1e97
chornate <155074527+chornate@users.noreply.github.com>
2024-11-10 22:10:12 +0300

    Update query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,10 +2,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
-    # Use get to retrieve the Author object, or handle the case where the author doesn't exist
     try:
         author = Author.objects.get(name=author_name)
-        # Filter books by the retrieved author
         books = Book.objects.filter(author=author)
         return books
     except Author.DoesNotExist:
@@ -13,14 +11,13 @@ def get_books_by_author(author_name):
 
 # List all books in a library
 def get_books_in_library(library_name):
-    # Retrieve the library by name
     library = Library.objects.get(name=library_name)
-    # Return all books in the library
     return library.books.all()
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    # Retrieve the library
+    # Retrieve the library object
     library = Library.objects.get(name=library_name)
-    # Return the librarian assigned to the library
-    return library.librarian
+    # Use the OneToOne relationship to get the librarian for the library
+    librarian = Librarian.objects.get(library=library)
+    return librarian
