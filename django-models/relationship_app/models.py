939ca430ab404e66a5e4c19470e012e68eb24d76
chornate <155074527+chornate@users.noreply.github.com>
2024-11-10 23:57:53 +0300

    updated urls.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -40,3 +40,37 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    # Optionally, add other fields like birthdate, biography, etc.
+    
+    def __str__(self):
+        return self.name
+
+# Book Model
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField()
+    genre = models.CharField(max_length=100, blank=True, null=True)
+    # Reference to Librarian: Assuming a librarian manages the book
+    librarian = models.ForeignKey('Librarian', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can edit book'),
+            ('can_delete_book', 'Can delete book')
+        ]
+
+# Librarian Model
+class Librarian(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    library_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Librarian {self.user.username} at {self.library_name}"
