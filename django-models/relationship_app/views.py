cb7c72b8d12c406df7d88733d50ed6bcbf670d9b
chornate <155074527+chornate@users.noreply.github.com>
2024-11-10 23:32:47 +0300

    Added user authentication with login, logout, and registration views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,47 +1,33 @@
-from django.contrib.auth import views as auth_views
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Library
-from .models import Book  
-from django.views.generic.detail import DetailView
-from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
-from django.contrib.auth.views import LoginView, LogoutView
+from .models import Library
+from .models import Book
 
-from .models import Book, Library
 
-
+# Class-based view for displaying library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
 
+# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
+# User registration view
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
-            form.save()  
-            return redirect('login')  
+            return redirect('login')  # Redirect to login after successful registration
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
-
-def list_books(request):
-    books = Book.objects.all()  
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-class LibraryDetailView(DetailView):
-    model = Library  
-    template_name = 'relationship_app/library_detail.html'  
-    context_object_name = 'library'  
